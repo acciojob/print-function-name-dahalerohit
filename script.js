6772b6ac6879ecd3fc36bffbef48db67e3bf3ab9
Rohit Dahale <151941395+dahalerohit@users.noreply.github.com>
2025-08-31 04:50:34 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,12 @@
 //your JS code here. If required.
+function functionName();
+{
+	alert(arguments.callee.name);
+}
+function Acciojob();
+{
+	alert(arguments.callee.name);
+}
+
+functionName();
+Acciojob();
